ab39dc9d023a648ee484f128c9388dede775be14
markl <markl@L146.ehi.ehealth.com>
2016-06-28 10:55:06 +0800

    First Web

---
+++ src/main/java/com/firstspring/first_web_development/FirstWebApp.java
@@ -0,0 +1,22 @@
+package com.firstspring.first_web_development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@SpringBootApplication
+@Controller
+public class FirstWebApp {
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Hello World!";
+	}
+	
+	public static void main (String[] args) {
+		SpringApplication.run(FirstWebApp.class, args);
+	}
+}
